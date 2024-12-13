7e6d42ecdfcc1e85b1774a05ee74a57024aaddb3
seno <seno@cloud-ace.jp>
2024-12-13 14:40:51 +0900

    fix author

--- init.rb
+++ init.rb
@@ -4,11 +4,11 @@ require File.expand_path('../lib/redmine_slack/listener', __FILE__)
 
 Redmine::Plugin.register :redmine_slack do
 	name 'Redmine Slack'
-	author 'Samuel Cormier-Iijima'
-	url 'https://github.com/sciyoshi/redmine-slack'
-	author_url 'http://www.sciyoshi.com'
+	author '0Delta'
+	url 'https://github.com/0Delta/redmine-slack'
+	author_url 'https://github.com/0Delta'
 	description 'Slack chat integration'
-	version '0.2'
+	version '0.12'
 
 	requires_redmine :version_or_higher => '0.8.0'
 
@@ -16,8 +16,6 @@ Redmine::Plugin.register :redmine_slack do
 		:default => {
 			'callback_url' => 'http://slack.com/callback/',
 			'channel' => nil,
-			'icon' => 'https://raw.github.com/sciyoshi/redmine-slack/gh-pages/icon.png',
-			'username' => 'redmine',
 			'display_watchers' => 'no'
 		},
 		:partial => 'settings/slack_settings'
